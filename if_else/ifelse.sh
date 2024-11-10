34967d96d2f3fbeb9da69652524d70ce9d9f5348
Pe-te-r <mburupeter8526@gmail.com>
2024-11-11 00:19:44 +0300

    if else done

--- if_else/ifelse.sh
+++ if_else/ifelse.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-money=200
+# money=200
 
-if [ $money -eq 200 ]; then
-  echo "this is true"
+# if [ $money -eq 200 ]; then
+  # echo "this is true"
+# else
+  # echo "money is not 200"
+#!/bin/bash
+
+command=/usr/bin/htop
+
+if [ -f $command ]; then
+  echo "Command is already available let's run it now."
+else
+ echo "Installing command first"
+ sudo pacman -Syu htop
+
 fi
+
+$command
